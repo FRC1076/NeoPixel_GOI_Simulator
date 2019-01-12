2ecce793cf918195cef5807e196b61304af5c6c1
EPrograming <ethds24@gmail.com>
2019-01-12 16:33:49 -0500

    fix

--- NeoDisplay.py
+++ NeoDisplay.py
@@ -11,39 +11,41 @@ class NeoDisplay:
         They should print 
         """
 
-        #if (error_message != "Ok"):
-        #                            1                   2                   3
-        #        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-        return '+---------------------------------------------------------------+\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                         No Data                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '|                                                               |\n' + \
-            '+---------------------------------------------------------------+\n' + \
-            ' Status: '+error_message
-        #else:
-        #values = self.pixels
+        if (error_message == "No Data" or self.pixels == []):
+            #                        1                   2                   3
+            #        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+            return '+---------------------------------------------------------------+\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                         No Data                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '|                                                               |\n' + \
+                '+---------------------------------------------------------------+\n' + \
+                ' Status: '+error_message
+        else:
+            pixels = self.pixels
+            print(pixels)
 
-        #line0 = [0, 2]
+            #line0 = [0, 2]
 
-        #    line_values = self.pixels
+            #line_values = []
 
-        #for positions in line0:
-        #    line_values.append(values[positions])
-        #    rendered_line = '| ' + ' '.join(line_values) + ' |'
-        #    return (rendered_line)
+            #for positions in line0:
+            #    line_values.append(values[positions])
+            #rendered_line = '| ' + ' '.join(line_values) + ' |'
+            #return (rendered_line)
+            return 0
 
     def set_pixels(self, pixels_to_set):
         """
