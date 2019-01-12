0062fafe9247db01145f065c33a763835ee07498
EPrograming <ethds24@gmail.com>
2019-01-12 15:15:10 -0500

    Fixing issues

--- NeoDisplay.py
+++ NeoDisplay.py
@@ -1,7 +1,6 @@
 class NeoDisplay:
-
     def __init__(self, num_pixels):
-        pass
+        self.pixels = []
 
     def clear(self):
         pass
@@ -11,35 +10,44 @@ class NeoDisplay:
         Return the string rendering of the NeoDisplay contents.
         They should print 
         """
-        
-        if(error_message != "Ok"):
-            #                            1                   2                   3
-            #        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-            return '+---------------------------------------------------------------+\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                         No Data                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '|                                                               |\n' + \
-                '+---------------------------------------------------------------+\n' + \
-                ' Status: '+error_message 
-        else:
-                
+
+        #if (error_message != "Ok"):
+        #                            1                   2                   3
+        #        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        return '+---------------------------------------------------------------+\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                         No Data                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '|                                                               |\n' + \
+            '+---------------------------------------------------------------+\n' + \
+            ' Status: '+error_message
+        #else:
+        #values = self.pixels
+
+        #line0 = [0, 2]
+
+        #    line_values = self.pixels
+
+        #for positions in line0:
+        #    line_values.append(values[positions])
+        #    rendered_line = '| ' + ' '.join(line_values) + ' |'
+        #    return (rendered_line)
 
     def set_pixels(self, pixels_to_set):
         """
         Given an array of pixels indices, turn on the pixel
         in that position.
         """
-        pass
+        self.pixels = pixels_to_set
--- neo_goi.py
+++ neo_goi.py
@@ -1,14 +1,13 @@
-
 try:
     from frc1076lib.udp_channel import UDPChannel as UDPChannel
-except :
+except:
     from lib1076.udp_channel import UDPChannel as UDPChannel
 
 #Ignore error here
 from NeoDisplay import NeoDisplay
 import time
 import json
-import sys,argparse
+import sys, argparse
 #
 #   If the user sets the terminal window to 64 wide and 12 pixels
 #   high the display will fill the window.   Set the font to fixed
@@ -25,17 +24,19 @@ REMOTE_PORT = 8877
 LOCAL_IP = 'localhost'
 LOCAL_PORT = 8876
 
+
 # some VT100 terminal helpers.
 def clear_screen():
-    print(chr(27)+'[2J', end='')
- 
+    print(chr(27) + '[2J', end='')
+
+
 def cursor_home():
-    print(chr(27)+'[H', end='')
+    print(chr(27) + '[H', end='')
 
 
 #Deal with arguments
-arguments = len(sys.argv) - 1  
-parser = argparse.ArgumentParser()  
+arguments = len(sys.argv) - 1
+parser = argparse.ArgumentParser()
 parser.add_argument("-LP", "--localport", help="sets localport")
 parser.add_argument("-RP", "--remoteport", help="sets remoteport")
 parser.add_argument("-LI", "--localip", help="sets localip")
@@ -44,20 +45,18 @@ parser.add_argument("-RI", "--remoteip", help="sets remoteip")
 args = parser.parse_args()
 
 # check for --version or -V
-if args.localport:  
+if args.localport:
     print("set local port to %s" % args.localport)
     LOCAL_PORT = args.localport
-elif args.remoteport:  
+elif args.remoteport:
     print("set remote port to %s" % args.remoteport)
     REMOTE_PORT = args.remote_port
-elif args.remoteip:  
+elif args.remoteip:
     print("set remote ip to %s" % args.remoteip)
     REMOTE_IP = args.remoteip
-elif args.localip:  
+elif args.localip:
     print("set local ip to %s" % args.localip)
     LOCAL_IP = args.local_ip
-
-
 
 # create the udp listener/receiver to read udp packets
 # from port LOCAL_PORT
@@ -66,10 +65,6 @@ receiver = UDPChannel(
     local_port=LOCAL_PORT,
     remote_ip=REMOTE_IP,
     remote_port=REMOTE_PORT)
-
-
-
-
 
 # create the display, initialized to ' '
 # just an array of characters seems fine as an implementation
@@ -81,21 +76,23 @@ print(display.render())
 
 # Loop forever
 while 1:
-
-    (message,(recv_addr,recv_port)) = receiver.receive_from()
+    if (receiver.receive_from() == (None, None)):
+        continue
+    else:
+        (message, (recv_addr, recv_port)) = receiver.receive_from()
 
     # read the incoming udp packet
     if message is not None:
-        
+
         # Test this to see what exception it generates
         # if the message is not valid json.  Then add
         # try/except so we can report a problem instead
         # of blindly trying to update the display.
         d = json.loads(message)
-        
+
         # extract the values from the resulting dictionary
         clear_directive = False
-        if(d['clear'] == 1):
+        if (d['clear'] == 1):
             clear_directive = True
 
         # we should probably check the message to make sure that
@@ -103,9 +100,9 @@ while 1:
         sender = d['sender']
         message = d['message']
         num_pixels = d['num_pixels']
-        pixel_values = d['pixel_values']            
-        
-        # if there is a "clear" directive, clear the display 
+        pixel_values = d['pixel_values']
+
+        # if there is a "clear" directive, clear the display
         if clear_directive:
             display.clear()
 
@@ -114,11 +111,9 @@ while 1:
         display.set_pixels(pixel_values)
 
         cursor_home()
-        print(display.render(
-                'Received ' +
-                str(pixel_values)+' from ('  +
-                str(recv_addr)+',' +
-                str(recv_port)+')'))
+        print(
+            display.render('Received ' + str(pixel_values) + ' from (' +
+                           str(recv_addr) + ',' + str(recv_port) + ')'))
 
     #
     # wait for a second before trying to read again
