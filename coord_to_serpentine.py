5c9522973d4dc06fe7528f1cece0a409b263b0a7
EPrograming <ethds24@gmail.com>
2019-01-26 14:30:03 -0500

    Finalized coord_to_serpentine function

--- coord_to_serpentine.py
+++ coord_to_serpentine.py
@@ -9,20 +9,13 @@ def coord_to_serpentine(coord_list):
         column = (current_coord % 8) + 1
         row = (int(current_coord / 8)) + 1
 
-        # print(row)
-        #print(column)
-        x = ((column - 1) * 8)
+        serpentine_value = ((column - 1) * 8)
         if ((column % 2) == 0):  #column is even
-
-            print("even")
-            x += ((8 - row) + 1) - 1
+            serpentine_value += ((8 - row) + 1) - 1
         else:  #column is odd
-            x += row - 1
-        #Check if even or odd
-        #if even go down
-        #if odd go up
-        print(x)
+            serpentine_value += row - 1
+        return serpentine_value
 
 
-coord_to_serpentine([34])
+print(coord_to_serpentine([10, 9]))
 #final result should be 20
